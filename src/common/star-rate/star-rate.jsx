31b059f44b47e4bd5444ab6101121d79843cd778
Muhiddin <muhiddin@MacBook-Air-Muhiddin.local>
2022-08-05 18:26:23 +0500

    stars by muhiddin

--- src/common/star-rate/star-rate.jsx
+++ src/common/star-rate/star-rate.jsx
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
-import { EmptyStar, FilledStar } from "../../assets/icons";
+import { EmptyStar, FilledStar, HalfStar } from "../../assets/icons";
 import "./star-rate.scss";
 
 export class StarRate extends Component {
   render() {
+    let prop = 3.5; //shetga 3.5 urniga this.props dan kelgan qiymat teng buladi
+    let filled = new Array(Math.floor(prop)).fill(1);
+    let half = prop - Math.floor(prop) === 0.5 && 1;
+    let empty = new Array(5 - half - filled.length).fill(1);
+
     return (
       <div className="starRate">
-        {FilledStar}
-        {FilledStar}
-        {FilledStar}
-        {EmptyStar}
-        {EmptyStar}
+        {filled.map(() => FilledStar)}
+        {half && HalfStar}
+        {empty.map(() => EmptyStar)}
       </div>
     );
   }
