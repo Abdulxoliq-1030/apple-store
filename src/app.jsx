0340934074950eb14a5fe50be7210d9db5287adc
Muhiddin <muhiddin@MacBook-Air-Muhiddin.local>
2022-08-05 18:31:23 +0500

    stars by muhiddin v8

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,10 @@
 import { Component } from "react";
-import Button from "./common/button/button";
-import { Heart } from "./assets/icons/index";
 
 class App extends Component {
   state = {};
 
   render() {
-    return (
-      <>
-      
-      </>
-    );
+    return <></>;
   }
 }
 export default App;
