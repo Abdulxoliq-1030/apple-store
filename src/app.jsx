0bf0c61c1dcc50434d20f8e7e1ef7f675c87de39
Abdulxoliq <odiljonovabdulxoliq11@gmail.com>
2022-08-05 17:40:36 +0500

    button px added

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,16 @@
 import { Component } from "react";
+import Button from "./common/button/button";
+import { Heart } from "./assets/icons/index";
 
 class App extends Component {
   state = {};
 
   render() {
-    return <></>;
+    return (
+      <>
+      
+      </>
+    );
   }
 }
 export default App;
