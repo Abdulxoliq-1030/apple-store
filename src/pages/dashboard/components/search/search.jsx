a274d82d5ed4d0605a9b08f6bd3c68795b6e3498
sulaymonov2002 <ssamir.web2002@gmail.com>
2022-08-06 16:28:41 +0500

    first codes fiter search

--- src/pages/dashboard/components/search/search.jsx
+++ src/pages/dashboard/components/search/search.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
-import "./search.scss"
+import "./seabrch.scss";
 class Search extends Component {
   render() {
     return (
       <div className="search">
-       <input type="text" placeholder="Apple Watch, Samsung S21, Mackbook Pro,..." />
+        <input
+          type="text"
+          placeholder="Apple Watch, Samsung S21, Mackbook Pro,..."
+        />
       </div>
     );
   }
