8ca0e638f5778ac2c65fcb133e72688e8f197c55
Muhiddin <muhiddin@MacBook-Air-Muhiddin.local>
2022-08-06 13:20:12 +0500

    blabla

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,7 @@ import { Component } from "react";
 
 import Login from "./pages/login/login";
 import "./assets/styles/base.scss";
+import Dashboard from "./pages/dashboard/dashboard";
 
 class App extends Component {
   state = {};
@@ -9,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <>
-        <Login />
+        <Dashboard />
       </>
     );
   }
