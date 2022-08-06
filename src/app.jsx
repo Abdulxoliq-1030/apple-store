2a5fdf358ba65fae4d2d3810f53c220f46c61947
Muhiddin <muhiddin@MacBook-Air-Muhiddin.local>
2022-08-06 13:35:56 +0500

    view tug'illndi

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,7 @@ import { Component } from "react";
 import Login from "./pages/login/login";
 import "./assets/styles/base.scss";
 import Dashboard from "./pages/dashboard/dashboard";
+import View from "./pages/view/view";
 
 class App extends Component {
   state = {};
@@ -9,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <>
-        <Dashboard />
+        <View />
       </>
     );
   }
